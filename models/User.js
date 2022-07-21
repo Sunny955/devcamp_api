47e1a3132ec2132b38e8b15dffbe4ec4ad57057c
Sunny955 <sunnyranjan9050@gmail.com>
2022-07-21 20:45:51 +0530

    Added cookie with token

--- models/User.js
+++ models/User.js
@@ -43,6 +43,7 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 // Sign JWT and return
