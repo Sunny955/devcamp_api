f19651da14f9bd983c1bf9eff7ce4b8a6e11e5c9
Sunny955 <sunnyranjan9050@gmail.com>
2022-07-31 00:54:01 +0530

    Added helmet,mongoSanitize and Xss checks

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -35,6 +38,15 @@ if (process.env.NODE_ENV === "development") {
 
 //File Uploading
 app.use(fileUpload());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevents XSS attacks
+app.use(xss());
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
